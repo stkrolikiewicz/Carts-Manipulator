e3d9b472328d4321c0ea82d8c0f6baf637a68413
stachkrol <st.krolikiewicz@gmail.com>
2023-04-13 08:22:06 +0200

    Fix display of price after discount
    The item's discounted price should be divided
    by the quantity of products, otherwise the
    displayed price is possibly few times bigger
    than expected.

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -8,13 +8,14 @@ type Props = {
   item: Product
 }
 const ProductCard = ({ item }: Props) => {
+  const discountedPrice = Number((item.discountedPrice / item.quantity).toFixed(2))
   return (
     <div className={styles.container}>
       <h3>{item.title}</h3>
       <p>product id: {item.id}</p>
       <p className={clsx(styles.tagline, 'line-through')}>price: {item.price}</p>
-      <h6 className={clsx(styles.tagline, (item.discountedPrice > item.price) ? 'text-red-600' : 'text-green-600')}>discounted price: {item.discountedPrice}</h6>
-      <p className='flex items-center gap-1'>discount: {item.discountPercentage} % {(item.discountedPrice > item.price) ? <ArrowUpIcon className='h-[1em] text-red-600' /> : <ArrowDownIcon className='h-[1em] text-green-600' />}</p>
+      <h6 className={clsx(styles.tagline, (discountedPrice > item.price) ? 'text-red-600' : 'text-green-600')}>price after discount: {discountedPrice}</h6>
+      <p className='flex items-center gap-1'>discount: {item.discountPercentage} % {(discountedPrice > item.price) ? <ArrowUpIcon className='h-[1em] text-red-600' /> : <ArrowDownIcon className='h-[1em] text-green-600' />}</p>
     </div>
   )
 }
